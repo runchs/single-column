966a355d7e569cecad2f5f026b43d171c27c26f8
runchs <runchida.ps@gmail.com>
2025-04-03 23:21:54 +0700

    [Update] adjust login page

--- src/views/InformationView.tsx
+++ src/views/InformationView.tsx
@@ -5,18 +5,18 @@ import Tabs from 'react-bootstrap/Tabs';
 import C360Tab from "../components/tabs/C360Tab";
 import EmproTab from "../components/tabs/EmproTab";
 
-const informationView: React.FC = () => {
+const InformationView: React.FC = () => {
   return (
-    <div>
+    <div className="h-100">
       <Tabs
         defaultActiveKey="c360"
         id="information-view"
         className="tabs-wrp"
       >
-        <Tab eventKey="empro" title="EMPRO">
+        <Tab eventKey="empro" title="EMPRO" className="h-100">
           <div className="tab-content-wrp"><EmproTab /></div>
         </Tab>
-        <Tab eventKey="c360" title="c360">
+        <Tab eventKey="c360" title="c360" className="h-100">
           <div className="tab-content-wrp"><C360Tab /></div>
         </Tab>
       </Tabs>
@@ -24,4 +24,4 @@ const informationView: React.FC = () => {
   );
 };
 
-export default informationView;
+export default InformationView;
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,43 +1,62 @@
 import React from "react";
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
 import Logo from '../assets/artworks/logoLeft.jpg';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
 
 const LoginView: React.FC = () => {
 
   const navigate = useNavigate();
 
+  function AlertDismissibleExample() {
+    const [show, setShow] = useState(true);
+  
+    if (show) {
+      return (
+        <Alert variant="danger" className="text-start fw-light mb-4">
+          <div >The username or password you entered is incorrect.</div>
+        </Alert>
+      );
+    }
+    return <Button onClick={() => setShow(true)}>Show Alert</Button>;
+  }
+
   return (
-    <Row className="gx-0 h-100">
-      <Col xs={7} className="bg-logo d-flex align-items-center"><img src={Logo} alt="logo" className="w-100" /></Col>
+    <Row className="gx-0 h-100 login-view-container ">
+      <Col xs={7} className="logo-wrp d-flex align-items-center"><img src={Logo} alt="logo" /></Col>
       <Col xs={5} className="d-flex align-items-center justify-content-center">
-        <div className="login-form fw-bold w-100 p-5">
+        <div className="fw-bold w-75 form-wrp">
           <div className="fs-1 mb-5">
             Sign in
           </div>
-          <div className="m-5">
+          <div>
+            {AlertDismissibleExample()}
+          </div>
+          <div>
             <Row className="d-flex align-items-center mb-3 gx-0">
-              <Col xs={3} className="text-start"><Form.Label htmlFor="username" className="mb-0">Username</Form.Label></Col>
+              <Col xs={3} className="text-start label-wrp"><Form.Label htmlFor="username" className="mb-0">Username</Form.Label></Col>
               <Col xs={9}><Form.Control
                 type="text"
                 id="username"
-                className="w-100 box-shadow-none"
+                className="w-100"
               /></Col>
             </Row>
             <Row className="d-flex align-items-center mb-3 gx-0">
-              <Col xs={3} className="text-start"><Form.Label htmlFor="password" className="mb-0">Password</Form.Label></Col>
+              <Col xs={3} className="text-start label-wrp"><Form.Label htmlFor="password" className="mb-0">Password</Form.Label></Col>
               <Col xs={9}><Form.Control
                 type="password"
                 id="password"
-                className="w-100 box-shadow-none"
+                className="w-100"
               /></Col>
             </Row>
+            <div className="text-start mt-5">
+              <Button variant="primary" onClick={() => navigate("/information")}>Sign in</Button>
+            </div>
           </div>
-          <div className="text-start mx-5">
-            <Button variant="primary" onClick={() => navigate("/information")}>Sign in</Button>
-          </div>
+
         </div>
 
       </Col>
